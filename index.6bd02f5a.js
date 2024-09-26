c7410e44cbe9093bb753c796cb67be186abcf419
Lucky Maurya <118681697+luckymaurya512@users.noreply.github.com>
2024-09-26 12:10:19 +0000

    Updates

---
+++ index.6bd02f5a.js
@@ -0,0 +1,27 @@
+// const heading=React.createElement(
+// "h1",{id : "heading"},
+//  "Hello World from  React!");
+// const root=ReactDOM.createRoot(
+// document.getElementById("root"));
+// root.render(heading);
+const parents = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I'm a h1 tag!"),
+        React.createElement("h2", {}, "I'm a h2 tag!")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I'm a h1 tag!"),
+        React.createElement("h2", {}, "I'm a h2 tag!")
+    ])
+]);
+console.log(parents);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parents);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
